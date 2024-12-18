0fa06493bc9a40cf578b1c64138da3d8c36a4f64
Ridge2 <root@Ridges-MacBook-Air.local>
2024-12-18 13:42:06 -0500

    added code to chcek email

--- imageToText.py
+++ imageToText.py
@@ -5,7 +5,8 @@ import pytesseract
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Load the image
-image_path = 'picForTextToImage.jpg'
+#image_path = 'picForTextToImage.jpg'
+image_path = input("Enter image path to convert: ")
 image = Image.open(image_path)
 
 # Extract text from the image
